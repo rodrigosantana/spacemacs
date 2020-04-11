12e479a8e8fed3d6fedcab084a276ca65892381d
Rodrigo Sant'Ana <oc.rodrigosantana@gmail.com>
2020-04-10 22:02:44 -0300

    Including header2 to .spacemacs

--- spacemacs.el
+++ spacemacs.el
@@ -65,6 +65,7 @@ values."
           ess-enable-smartparens t)
      funk
      polymode
+     header2
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
